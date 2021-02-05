7f88f6d57738f1a213b7387aadadbfa52f0ca9cf
Darius Raazi <dariusraazi@icloud.com>
2021-02-05 10:20:53 -0700

    deployment commit

--- front-end/app.js
+++ front-end/app.js
@@ -2,14 +2,77 @@
 const container = document.getElementById('container')
 const resultsDiv = document.getElementById("result-container")
 const searchBtn = document.getElementById('search-button')
+// console.log('howdy')
+
+localStorage.setItem('favoriteMovieArr', [])
 
 const consoleValue = value => {
     const searchValue = document.getElementById('search-input').value
-    return searchValue
+    console.log(searchValue)
+    fetch(`http://www.omdbapi.com/?apikey=ca9a8849&t=${searchValue}`)
+.then((responseData) => {
+    console.log(responseData)
+    return responseData.json()
+})
+.then((jsonData) => {
+    console.log(jsonData)
+        console.log('api search click')
+        resultsDiv.innerHTML = `
+            <div class='returned-results'>
+                <h3 id='result-title' class='result-title'>${jsonData.Title}</h3>
+                <img src="${jsonData.Poster}" alt="poster">
+
+                    <p id='rated' class='result'>Rated: ${jsonData.Rated}</p>
+                    <p id='plot' class='result'><i>${jsonData.Plot}</i></p>
+                    <p id='released' class='result'>Released: ${jsonData.Released}</p>
+                    <p id='runtime' class='result'>Runtime: ${jsonData.Runtime}</p>
+                
+                <div class='info-row'>
+                    <p id='genre' class='result'>Genre: ${jsonData.Genre}</p>
+                    <p id='director' class='result'>Director: ${jsonData.Director}</p>
+                    <p id='actors' class='result'>Actors: ${jsonData.Actors}</p>
+                    <p id='awards' class='result'>Awards: ${jsonData.Awards}</p>
+                <div class='ratings-container'>
+                    <p id='imd' class='result'>${jsonData.Ratings[0].Source}:</p>
+                    <p class='result-score'>${jsonData.Ratings[0].Value}</p>
+                </div>
+                <div class='ratings-container'>
+                    <p id='rotten-tomatoes' class='result'>${jsonData.Ratings[1].Source}:</p>
+                    <p class='result-score'>${jsonData.Ratings[1].Value}</p>
+                </div>
+                <div class='ratings-container'>
+                    <p id='metacritic' class='result'>${jsonData.Ratings[2].Source}:</p>
+                    <p class='result-score'>${jsonData.Ratings[2].Value}</p>
+                </div>
+                <div class='info-row'>
+                    <p id='imdb-rating' class='result'>IMDB Rating: ${jsonData.imdbRating}</p>
+                    <p id='box-office' class='result'>Box Office Gross: ${jsonData.BoxOffice}</p>
+                    <p id='production' class='result'>Production: ${jsonData.Production}</p>
+                    <p id='website' class='result'><a href="${jsonData.Website}" target="_blank" rel="noopener noreferrer">Movie Website</a>
+                </div>
+            </div>
+        `  
+    
+})
+.catch((error) => {
+    console.error(`There has been an error: ${error}`)
+})
 }
 
 searchBtn.addEventListener('click', consoleValue)
 
+// on page load the user should see a list a recently released movies underneath search
+
+// user needs to be able to search for movies
+
+// example of a helper function
+// let students = ['bob', 'jill', 'danny']
+// function setArrayToLS(arr){
+//     let JSONarr = JSON.stringify(arr)
+//     localStorage.setItem(`${name}, arr`)
+// }
+
+// setArrayToLS(students)
 // console.log(searchValue)
 
 // console.log(resultsDiv)
@@ -114,64 +177,3 @@ searchBtn.addEventListener('click', consoleValue)
 // document.addEventListener("click", function(e) {
 //     closeAllLists(e.target)
 // })
-
-fetch('http://www.omdbapi.com/?apikey=ca9a8849&t=indiana+jones')
-.then((responseData) => {
-    return responseData.json()
-})
-.then((jsonData) => {
-    console.log(jsonData)
-    searchBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        resultsDiv.innerHTML = `
-            <div class='returned-results'>
-                <h3 id='result-title' class='result-title'>${jsonData.Title}</h3>
-                <img src="${jsonData.Poster}" alt="poster">
-
-                    <p id='rated' class='result'>Rated: ${jsonData.Rated}</p>
-                    <p id='plot' class='result'><i>${jsonData.Plot}</i></p>
-                    <p id='released' class='result'>Released: ${jsonData.Released}</p>
-                    <p id='runtime' class='result'>Runtime: ${jsonData.Runtime}</p>
-                
-                <div class='info-row'>
-                    <p id='genre' class='result'>Genre: ${jsonData.Genre}</p>
-                    <p id='director' class='result'>Director: ${jsonData.Director}</p>
-                    <p id='actors' class='result'>Actors: ${jsonData.Actors}</p>
-                    <p id='awards' class='result'>Awards: ${jsonData.Awards}</p>
-                <div class='ratings-container'>
-                    <p id='imd' class='result'>${jsonData.Ratings[0].Source}:</p>
-                    <p class='result-score'>${jsonData.Ratings[0].Value}</p>
-                </div>
-                <div class='ratings-container'>
-                    <p id='rotten-tomatoes' class='result'>${jsonData.Ratings[1].Source}:</p>
-                    <p class='result-score'>${jsonData.Ratings[1].Value}</p>
-                </div>
-                <div class='ratings-container'>
-                    <p id='metacritic' class='result'>${jsonData.Ratings[2].Source}:</p>
-                    <p class='result-score'>${jsonData.Ratings[2].Value}</p>
-                </div>
-                <div class='info-row'>
-                    <p id='imdb-rating' class='result'>IMDB Rating: ${jsonData.imdbRating}</p>
-                    <p id='box-office' class='result'>Box Office Gross: ${jsonData.BoxOffice}</p>
-                    <p id='production' class='result'>Production: ${jsonData.Production}</p>
-                    <p id='website' class='result'><a href="${jsonData.Website}" target="_blank" rel="noopener noreferrer">Movie Website</a>
-                </div>
-            </div>
-        `  
-    })
-})
-.catch((error) => {
-    console.error(`There has been an error: ${error}`)
-})
-// on page load the user should see a list a recently released movies underneath search
-
-// user needs to be able to search for movies
-
-// example of a helper function
-// let students = ['bob', 'jill', 'danny']
-// function setArrayToLS(arr){
-//     let JSONarr = JSON.stringify(arr)
-//     localStorage.setItem(`${name}, arr`)
-// }
-
-// setArrayToLS(students)
